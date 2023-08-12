3244ec30b3a5cfa47abdd896ae5353c6601d7108
Henry <henryvictor215@gmail.com>
2023-08-11 18:39:00 -0700

    no 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Description: prints single digits of base 10 from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int num;
+
+        num = 0;
+
+        while (num < 10)
+        {
+                printf("%d", num);
+                num++;
+        }
+        printf("\n");
+        return (0);
+}
