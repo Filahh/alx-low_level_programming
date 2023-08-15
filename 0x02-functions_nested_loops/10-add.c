cf98dc36a61d2cca4c45e481a50d8924d9fc4769
Henry <henryvictor215@gmail.com>
2023-08-15 11:09:32 -0700

    prints all natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return the result
+ * @a: first integer
+ * @b: second integer
+ * Return: interger
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting with 0
+ * Return: void
+ */
+void times_table(void)
+{
+	int row, col, num;
+
+	for (row = 0; row <= 9; row++)
+	{
+		_putchar('0');
+		for (col = 1; col <= 9; col++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			num = row * col;
+			if (num <= 9)
+				_putchar(' ');
+			else
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
