2697e79db7bc7e2335883bc871376e8f48f91cdc
Henry <henryvictor215@gmail.com>
2023-08-15 03:31:56 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - a program that prints putchar,tgen a new line
+ *return : 0
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
