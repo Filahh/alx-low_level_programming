452148b8a5d50b1c5b67986d01a59861e7ef9317
Henry <henryvictor215@gmail.com>
2023-08-15 11:24:05 -0700

    adv task error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,43 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_to_98 - print all natural numbers from n to 98
- * @n: the first number
- *
- * Return: The numbers
+ * print_times_table - Prints the times table of the input,
+ * starting with 0.
+ * @n: The value of the times table to be printed.
  */
-void print_to_98(int n)
+
+void print_times_table(int n)
+
 {
-	if (n >= 98)
+
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
 	{
-		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
+	for (num = 0; num <= n; num++)
+	{
+	_putchar('0');
+	for (mult = 1; mult <= n; mult++)
+	{
+	_putchar(',');
+	_putchar(' ');
+	prod = num * mult;
+	if (prod <= 99)
+	_putchar(' ');
+	if (prod <= 9)
+	_putchar(' ');
+	if (prod >= 100)
+	{
+	_putchar((prod / 100) + '0');
+	_putchar(((prod / 10)) % 10 + '0');
 	}
-	else
+	else if (prod <= 99 && prod >= 10)
 	{
-		while (n < 98)
-			printf("%d, ", n++);
-		printf("%d\n", n);
+	_putchar((prod / 10) + '0');
+	}
+	_putchar((prod % 10) + '0');
+	}
+	_putchar('\n');
+	}
 	}
 }
