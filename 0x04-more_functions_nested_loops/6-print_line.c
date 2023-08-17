372b5a6f3cd61726c11715a07fb9e8804555bf7b
Henry <henryvictor215@gmail.com>
2023-08-17 15:56:53 -0700

    error

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * print_line - draws a straight line in the terminal
- * @n: a straight line		
+ * @n: a straight line
  * Return: Always 0
  */
+
 void print_line(int n)
 {
 	if (n <= 0)
