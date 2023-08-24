2dc97f0ccd697d38e7491f7fb9f32962ae4fb4d2
Henry <henryvictor215@gmail.com>
2023-08-24 05:05:28 -0700

    compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * strcmp - compares two strings
+ * 
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return:
+ * 	returns zero if s1 == s2
+ * 	returns negative number if s1 < s2
+ * 	returns positive number if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return strcmp(s1, s2);
+}
