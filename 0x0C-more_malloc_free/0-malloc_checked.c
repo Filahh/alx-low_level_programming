33103cd2b1f00fd1835adf4fd57c2c15e0b98d7a
Henry <henryvictor215@gmail.com>
2023-09-07 14:47:36 -0700

    allocates mem space

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <limits.h>
+/**
+ * malloc_checked - allocates a memory
+ * @b: integer
+ *
+ * Return: pointer or 98 if failed
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
