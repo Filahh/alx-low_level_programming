de0bb2512b0e448962ce2aa0dee630cb7009a17b
Henry <henryvictor215@gmail.com>
2023-09-26 16:43:40 -0700

    bugs fixed

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,19 +7,20 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *prev = NULL;
-    listint_t *current = *head;
-    
-    while (current != NULL)
-    {
-        listint_t *next_node = current->next;
-        current->next = prev;
-        prev = current;
-        current = next_node;
-    }
-    
-    *head = prev; // Update the head to point to the new first node
-    
-    return prev;
+	listint_t *prev = NULL;
+
+	listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		listint_t *next_node = current->next;
+
+		current->next = prev;
+		prev = current;
+		current = next_node;
+	}
+
+	*head = prev;
+	return (prev);
 }
 
